3536c9966b4ba73d0789d68d7d5673775c49f796
rishihe <nagderishi@gmail.com>
2026-06-14 11:00:34 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 const app = require("./app")
+const connectDatabase = require("./config/database.js")
 
 const dotenv = require("dotenv")
 dotenv.config({path: "./config/config.env"})
 
+connectDatabase();
+
 app.listen(process.env.PORT, () =>{
     console.log(`server started at port ${process.env.PORT}`)
-    
 })
