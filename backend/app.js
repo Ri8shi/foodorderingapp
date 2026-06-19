dfd541eae3f7ab8ddd3edeea458a33b410235ff0
rishihe <nagderishi@gmail.com>
2026-06-19 11:31:20 +0530

    basic for backend

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-
+const auth = require("./routes/auth")
 
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/v1/users", auth)
 module.exports = app; 
