86fbe56e0061795b456a933d70715e9c04cefa9a
rishihe <nagderishi@gmail.com>
2026-06-21 15:13:07 +0530

    update

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+
 const auth = require("./routes/auth")
+const restaurant = require("./routes/restaurant")
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1/users", auth)
+app.use("/api/v1/eat", restaurant)
+
+
 module.exports = app; 
