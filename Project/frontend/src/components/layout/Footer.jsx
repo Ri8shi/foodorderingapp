98f87481360033888a7528400dfdae8971d35a43
rishihe <nagderishi@gmail.com>
2026-07-13 18:01:22 +0530

    init

---
+++ Project/frontend/src/components/layout/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <p className="text-center">
+          Food Delivery Website , All Rights Reserved..
+        </p>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
